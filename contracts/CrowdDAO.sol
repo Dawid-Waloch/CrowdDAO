83292866e61c4e3e971f6402e1a3aed1a50e6a2c
Dawid <dawwal1900@gmail.com>
2025-07-29 22:55:20 +0200

    Creating token and added contribution function which mints tokens, states added to gitignore file

--- contracts/CrowdDAO.sol
+++ contracts/CrowdDAO.sol
@@ -1,6 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+interface IGovernanceToken {
+    function mint(address to, uint amount) external;
+}
+
 contract CrowdDAO {
-    
+    IGovernanceToken public token;
+
+    constructor(address tokenAddress) {
+        token = IGovernanceToken(tokenAddress);
+    }
+
+    function contribute() public payable {
+        require(msg.value > 0, 'You have to send some ETH to contribute');
+
+        token.mint(msg.sender, msg.value);
+    }
+
 }

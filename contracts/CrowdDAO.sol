ffe7cf46da89ebf088f206bf5d73871953043a39
Dawid <dawwal1900@gmail.com>
2025-07-30 21:33:55 +0200

    Rate 1 ETH = 100 Tokens added

--- contracts/CrowdDAO.sol
+++ contracts/CrowdDAO.sol
@@ -7,15 +7,18 @@ interface IGovernanceToken {
 
 contract CrowdDAO {
     IGovernanceToken public token;
+    uint rate;
 
     constructor(address tokenAddress) {
         token = IGovernanceToken(tokenAddress);
+        rate = 100 * (10 ** 18);
     }
 
     function contribute() public payable {
         require(msg.value > 0, 'You have to send some ETH to contribute');
 
-        token.mint(msg.sender, msg.value);
+        uint amount = msg.value * rate;
+        token.mint(msg.sender, amount);
     }
 
 }

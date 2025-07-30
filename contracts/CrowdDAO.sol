f8c232dcd92531f10388fdbcc3ee5b4a4c3a14f1
Dawid <dawwal1900@gmail.com>
2025-07-30 21:51:56 +0200

    Rate corrected

--- contracts/CrowdDAO.sol
+++ contracts/CrowdDAO.sol
@@ -11,13 +11,13 @@ contract CrowdDAO {
 
     constructor(address tokenAddress) {
         token = IGovernanceToken(tokenAddress);
-        rate = 100 * (10 ** 18);
+        rate = 100;
     }
 
     function contribute() public payable {
         require(msg.value > 0, 'You have to send some ETH to contribute');
 
-        uint amount = msg.value * rate;
+        uint amount = (msg.value * rate) / 1 ether;
         token.mint(msg.sender, amount);
     }
 

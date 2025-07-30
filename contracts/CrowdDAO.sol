9780e3ff5217fadc202bb29912e5b6428dd20d75
Dawid <dawwal1900@gmail.com>
2025-07-30 22:24:39 +0200

    Adding createProposal function and struct where proposals will be stored

--- contracts/CrowdDAO.sol
+++ contracts/CrowdDAO.sol
@@ -3,11 +3,22 @@ pragma solidity ^0.8.0;
 
 interface IGovernanceToken {
     function mint(address to, uint amount) external;
+    function balanceOf(address whose) external view returns (uint);
 }
 
 contract CrowdDAO {
+    enum ProposalStatus { Otwarta, Zamknieta, Zrealizowana }
+    struct Proposal {
+        string description;
+        address recipient;
+        uint value;
+        uint[2] votes;
+        ProposalStatus status;
+    }
+
     IGovernanceToken public token;
-    uint rate;
+    uint public rate;
+    Proposal[] public proposals;
 
     constructor(address tokenAddress) {
         token = IGovernanceToken(tokenAddress);
@@ -19,6 +30,18 @@ contract CrowdDAO {
 
         uint amount = (msg.value * rate) / 1 ether;
         token.mint(msg.sender, amount);
+    }
+
+    function createProposal(string memory purpose, uint value) public {
+        require(token.balanceOf(msg.sender) > 0, "You don't have tokens so you can't create proposals");
+        Proposal memory newProposal = Proposal({
+            description: purpose,
+            recipient: msg.sender,
+            value: value,
+            votes: [uint(0), uint(0)],
+            status: ProposalStatus.Otwarta
+        });
+        proposals.push(newProposal);
     }
 
 }

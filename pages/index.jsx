1861ac27d1692acdb93fff7e8578bcbc9494f723
Dawid <dawwal1900@gmail.com>
2025-08-07 22:06:31 +0200

    Empty file structure added

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import getCrowdDAOContract from "../ethereum/dao";
+import getGovernanceTokenContract from "../ethereum/token";
+import Navbar from "../components/Navbar/Navbar";
 
 const HomePage = () => {
+    useEffect(() => {
+        const loadContracts = async () => {
+            const governanceTokenContract = await getGovernanceTokenContract();
+            const crowdDAOContract = await getCrowdDAOContract();
+        }
+
+        loadContracts();
+    }, []);
+
     return (
-        <h1>Witaj użytkowniku</h1>
+        <>        
+            <Navbar />
+            <h1>Witaj użytkowniku</h1>
+        </>
+
     );
 };
 

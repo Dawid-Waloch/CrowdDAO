9f4262c26d0b920f1cbc6a67f5a6753e1779f60d
Dawid <dawwal1900@gmail.com>
2025-08-12 20:22:03 +0200

    added proposals sample page to check pushing to route

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,19 +3,29 @@ import React from "react";
 import getCrowdDAOContract from "../ethereum/dao";
 import getGovernanceTokenContract from "../ethereum/token";
 import Navbar from "../components/Navbar/Navbar";
+import { PageWrapper } from "../components/PageWrapper";
+import { Button } from "../components/ButtonStyled";
+import { PageStyled } from "../components/PageStyled";
+import { useRouter } from "next/router";
 
 const HomePage = () => {
+    const router = useRouter();
     const loadContracts = async () => {
         const governanceTokenContract = await getGovernanceTokenContract();
         const crowdDAOContract = await getCrowdDAOContract();
+
+        router.push('/proposals');
     };
 
     return (
-        <>        
+        <PageStyled>        
             <Navbar />
-            <h1>Witaj użytkowniku</h1>
-            <button onClick={loadContracts}>Connect with the wallet</button>
-        </>
+            <PageWrapper>
+                <h1>👋 Welcome to CrowdDAO!</h1>
+                <p>Join the community and take part in decentralized organization voting.</p>
+                <Button onClick={loadContracts}>🔗 Connect Wallet</Button>
+            </PageWrapper>
+        </PageStyled>
 
     );
 }
---
+++ pages/proposals/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProposalsPage = () => {
+    return (
+        <h1>Hello everyone, this is proposals page</h1>
+    )
+};
+
+export default ProposalsPage;
